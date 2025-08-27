6d378c6a8967ce3af984238f42f78a2e383f8709
Salmin Lev <prostoweb123@gmail.com>
2025-08-27 11:46:41 +0600

    Create main layout.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './style.css';
+//import './style.css';
 
 import pinia from './store';
 import piniaPersistedstate from 'pinia-plugin-persistedstate';
