fc526e3f5c4a50f40db9b85cfa9772e8757f70fc
Salmin Lev <prostoweb123@gmail.com>
2025-11-06 17:04:27 +0600

    Fix edit page.

--- src/views/admin/lessons/LessonEdit.vue
+++ src/views/admin/lessons/LessonEdit.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted, reactive, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
-
-import { useCourseSectionStore, useCourseSectionStoreWithGetters } from '../../../store/admin/course_section/course_section.store.ts';
+import { useLessonStore, useLessonStoreWithGetters } from '../../../store/admin/lesson/lesson.store.ts';
 import { useErrorHandler } from '../../../composables/useErrorHandler';
 import api from '../../../api';
 
@@ -10,31 +9,37 @@ import BaseForm from '../../../components/ui/BaseForm.vue';
 import BaseInput from '../../../components/ui/BaseInput.vue';
 import BaseToggle from '../../../components/ui/BaseToggle.vue';
 import FormErrors from '../../../components/ui/FormErrors.vue';
+import BaseTextAreaWithEditor from "../../../components/ui/BaseTextAreaWithEditor.vue";
 
 const route = useRoute();
 const router = useRouter();
-const courseSectionId = Number(route.params.id);
+const lessonId = Number(route.params.id);
 
 // Используем store с геттерами
-const { currentCourseSection } = useCourseSectionStoreWithGetters();
+const { currentLesson } = useLessonStoreWithGetters();
 const { error, setError } = useErrorHandler();
 const loading = ref(false);
 
 // реактивная модель формы
 const formModel = reactive({
   name: '',
-  url: '',
+  content: '',
+  duration: '',
   status: 1,
-  course_id: null as number | null,
+  free_pay: 1,
+  course_section_id: null as number | null,
 });
 
 // при изменении currentMenu заполняем форму
-watch(currentCourseSection, (val) => {
+watch(currentLesson, (val) => {
   if (!val) return;
   Object.assign(formModel, {
     name: val.name,
+    content: val.content,
+    duration: val.duration,
     status: val.status ?? 1,
-    course_id: val.course_id ?? null,
+    free_pay: val.free_pay ?? 1,
+    course_section_id: val.course_section_id ?? null,
   });
 });
 
@@ -44,8 +49,8 @@ const save = async () => {
   error.value = null;
 
   try {
-    await api.put(`/admin/course-section/${courseSectionId}`, { ...formModel });
-    router.push('/admin/course-section');
+    await api.put(`/admin/lessons/${lessonId}`, { ...formModel });
+    router.push('/admin/lessons');
   } catch (e: any) {
     setError(e);
   } finally {
@@ -55,23 +60,32 @@ const save = async () => {
 
 // загрузка данных меню при монтировании
 onMounted(() => {
-  if (!isNaN(courseSectionId)) {
-    useCourseSectionStore().fetchItem(courseSectionId);
+  if (!isNaN(lessonId)) {
+    useLessonStore().fetchItem(lessonId);
   } else {
-    error.value = { general: ['Некорректный ID раздела курса'] };
+    error.value = { general: ['Некорректный ID урока'] };
   }
 });
 </script>
 
 <template>
-  <BaseForm label="Редактировать раздел курса" :loading="loading" :onSubmit="save">
+  <BaseForm label="Редактировать урок" :loading="loading" :onSubmit="save">
     <FormErrors :error="error" />
 
     <BaseInput v-model="formModel.name" label="Имя" required class="mb-4" />
+    <BaseTextAreaWithEditor v-model="formModel.content" label="Контент" required class="w-full mb-4" />
+    <BaseInput v-model="formModel.duration" label="Длительность" required class="mb-4" />
 
     <BaseToggle
         v-model="formModel.status"
         label="Статус"
+        :activeLabel="'Активный'"
+        :inactiveLabel="'Неактивный'"
+        class="mb-4"
+    />
+    <BaseToggle
+        v-model="formModel.free_pay"
+        label="Бесплатный просмотр"
         :activeLabel="'Активный'"
         :inactiveLabel="'Неактивный'"
         class="mb-4"
