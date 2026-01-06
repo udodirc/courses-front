6d8b6b473e8dda1cad9a0cc46fc52ffda39ab7c1
leva <prostoweb123@gmail.com>
2026-01-06 22:35:31 +0600

    Fix partner email verify.

--- src/views/front/auth/VerifyEmailView.vue
+++ src/views/front/auth/VerifyEmailView.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
-import { useRouter, useRoute } from 'vue-router';
+import { useRoute } from 'vue-router';
 
 const route = useRoute();
-const router = useRouter();
 
 onMounted(() => {
   const backendUrl = route.query.backend_url as string;
-  if (backendUrl) {
-    window.location.href = decodeURIComponent(backendUrl);
-  } else {
-    router.replace('/');
-  }
+  window.location.href = decodeURIComponent(backendUrl);
 });
 </script>
 
